f6beec9a85f06c71af7e4879376f393f36f021f1
lumses <ki.kashirin@yandex.ru>
2025-05-07 23:12:59 +0300

    refactor wrk_payload.lua

--- lab05/user_service/wrk/wrk_payload.lua
+++ lab05/user_service/wrk/wrk_payload.lua
@@ -1,4 +1,4 @@
 wrk.method = "POST"
 wrk.body = '{"fields": ["login"], "value": "ajohnson"}'
 wrk.headers["Content-Type"] = "application/json"
-wrk.headers["Authorization"] = "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhZG1pbiIsImV4cCI6MTc0NjY0OTk0MH0.Ay0K9HlQHZv2cLZo4NZZOsexz35TEzP229nNbsltyjM"
+wrk.headers["Authorization"] = "Bearer <TOKEN>"
